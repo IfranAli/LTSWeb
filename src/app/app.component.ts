edcc80eb97c573f44a5bf17a7b6afab211c82c8f
Ila <tehpanda32@gmail.com>
2023-04-16 21:16:55 +1000

    master: WIP implement sidebar toggle/hamburger menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,72 +1,89 @@
-import {ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit} from '@angular/core';
-import {UserModel} from "./models/user.interface";
-import {UserService} from "./services/user.service";
-import {Router} from "@angular/router";
-import {DataProviderService} from './services/data-provider.service';
-import {filter, Observable, of, Subscription, switchMap, tap} from 'rxjs';
-import {AppState} from './reducers';
-import {Store} from '@ngrx/store';
-import {loginUser} from './actions/user.actions';
-import * as fromUsers from './reducers/user.reducer';
-import {UserState} from './reducers/user.reducer';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnDestroy,
+  OnInit,
+} from "@angular/core";
+import { Router } from "@angular/router";
+import { Store } from "@ngrx/store";
+import { filter, Observable, of, Subscription, switchMap, tap } from "rxjs";
+import { loginUser } from "./actions/user.actions";
+import { UserModel } from "./models/user.interface";
+import { AppState } from "./reducers";
+import * as fromUsers from "./reducers/user.reducer";
+import { UserState } from "./reducers/user.reducer";
+import { DataProviderService } from "./services/data-provider.service";
+import { UserService } from "./services/user.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent implements OnInit, OnDestroy {
-  title = 'LTSweb';
+  title = "LTSweb";
   user: UserModel | null = null;
   dataProvider: DataProviderService;
-  $subscription: Subscription | undefined
+  $subscription: Subscription | undefined;
+  sidebarOpen = false;
 
-  $sub1: Observable<UserState> =
-    this.store.select<UserState>(fromUsers.selectUserState).pipe(
-      filter(user => !!user.user && user.user.id != 0),
+  $sub1: Observable<UserState> = this.store
+    .select<UserState>(fromUsers.selectUserState)
+    .pipe(
+      filter((user) => !!user.user && user.user.id != 0),
       switchMap((user) => {
         return of(user);
       }),
       tap((user) => {
-          this.user = user.user as UserModel;
-        }
-      ));
+        this.user = user.user as UserModel;
+      })
+    );
 
   constructor(
     private store: Store<AppState>,
     private userService: UserService,
-    private router: Router,
+    private router: Router
   ) {
     this.dataProvider = inject(DataProviderService);
   }
 
   ngOnInit() {
-    const tokenName = 'token';
+    const tokenName = "token";
     const token = localStorage.getItem(tokenName);
 
     if (!token) {
       this.userLogout();
-      this.store.dispatch(loginUser({id: 0, password: '', username: ''}));
+      this.store.dispatch(loginUser({ id: 0, password: "", username: "" }));
     } else {
-      this.$subscription = this.dataProvider.getUserStatus().subscribe(value => {
-        this.store.dispatch(loginUser({id: value.id, password: '', username: value.name}));
-      }, error => {
-        localStorage.removeItem(tokenName)
-        this.router.navigate([''])
-      })
+      this.$subscription = this.dataProvider.getUserStatus().subscribe(
+        (value) => {
+          this.store.dispatch(
+            loginUser({ id: value.id, password: "", username: value.name })
+          );
+        },
+        (error) => {
+          localStorage.removeItem(tokenName);
+          this.router.navigate([""]);
+        }
+      );
     }
   }
 
   userLogout() {
-    this.userService.logoutUser().subscribe(async value => {
-      localStorage.removeItem('token');
+    this.userService.logoutUser().subscribe(async (value) => {
+      localStorage.removeItem("token");
       this.user = null;
-      await this.router.navigate(['']);
-    })
+      await this.router.navigate([""]);
+    });
   }
 
   ngOnDestroy(): void {
     this.$subscription?.unsubscribe();
+  }
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
   }
 }
